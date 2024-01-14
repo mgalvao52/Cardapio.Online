db82e3b91ab8fc8ca5cd3c8b1720acfa6f07c757
marco galvao <marco.galvao@fatec.sp.gov.br>
2024-01-14 20:41:21 -0300

    add PaymentService

---
+++ Cardapio.Web.BlazorApp/Services/OrderPaymentService.cs
@@ -0,0 +1,34 @@
+﻿using Cardapio.Application.DTOs;
+
+namespace Cardapio.Web.BlazorApp.Services
+{
+    public class OrderPaymentService
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrderPaymentService(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient.CreateClient(nameof(MenuService));
+        }
+
+        public async Task<bool> AddPaymentAsync(AddPaymentDTO addPayment)
+        {
+            var result = false;
+            try
+            {
+                HttpResponseMessage resp = null;
+                resp = await _httpClient.PostAsJsonAsync($"api/payment", addPayment);
+                if (resp.IsSuccessStatusCode)
+                {
+                    var json = await resp.Content.ReadAsStringAsync();
+                }
+
+                return resp.IsSuccessStatusCode;
+            }
+            catch (Exception)
+            {
+            }
+            return result;
+        }
+    }
+}
